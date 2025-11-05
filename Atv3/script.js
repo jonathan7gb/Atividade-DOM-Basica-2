93694164265d310e8048526a4bb879e045ade2db
jonathan_uber <jonathanuber121207@gmail.com>
2025-11-05 15:08:35 -0300

    começa lógica de alterar cor titulo, paragrafo e background

--- Atv3/script.js
+++ Atv3/script.js
@@ -1,15 +1,32 @@
-const somar = document.getElementById("somar")
-const diminuir = document.getElementById("diminuir")
-const numero = document.getElementById("numero")
+const bodyHTML = document.getElementById("body")
+const titulo = document.getElementById("titulo")
+const paragrafo = document.getElementById("paragrafo")
+const cor_fundo = document.getElementById("cor_fundo")
+const cor_titulo = document.getElementById("cor_titulo")
+const cor_paragrafo = document.getElementById("cor_paragrafo")
+const fonte_titulo = document.getElementById("fonte_titulo")
+const fonte_paragrafo = document.getElementById("fonte_paragrafo")
+const alterar_tudo = document.getElementById("titulo")
 
-let valor = 0;
-somar.addEventListener('click', () => {
-    valor += 1
-    numero.textContent = valor 
+function gerarCorAleatoria(){
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r},${g},${b})`
+}
+
+cor_fundo.addEventListener('click', () => {
+    bodyHTML.style.backgroundColor = gerarCorAleatoria()
+    console.log("Cor alterada para: " + gerarCorAleatoria());
 })
 
-diminuir.addEventListener('click', () => {
-    valor -= 1
-    numero.textContent = valor
+cor_titulo.addEventListener('click', () => {
+    titulo.style.color = gerarCorAleatoria()
+    console.log("Cor alterada para: " + gerarCorAleatoria());
 })
 
+cor_paragrafo.addEventListener('click', () => {
+    paragrafo.style.color = gerarCorAleatoria()
+    console.log("Cor alterada para: " + gerarCorAleatoria());
+})
