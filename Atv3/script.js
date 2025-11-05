46be0fc7a3cdb4b953e63a465674369a34c50094
jonathan_uber <jonathanuber121207@gmail.com>
2025-11-05 15:22:32 -0300

    começa lógica de alterar fonte titulo, paragrafo

--- Atv3/script.js
+++ Atv3/script.js
@@ -7,15 +7,23 @@ const cor_paragrafo = document.getElementById("cor_paragrafo")
 const fonte_titulo = document.getElementById("fonte_titulo")
 const fonte_paragrafo = document.getElementById("fonte_paragrafo")
 const alterar_tudo = document.getElementById("titulo")
+const fontes = ['Roboto', 'Elms Sans', 'Noto Sans Syriac Western', 
+    'Momo Signature', 'Momo Trust Display', 'Momo Trust Sans', 'Stack Sans Notch', 
+    'Raleway', 'Noto Sans', 'Oswald', 'Kanit', 'Open Sans', 
+    'Work Sans', 'Lavishly Yours', 'Google Sans Code']
+    
+// ===============================
+// FUNÇÕES PARA ALTERAR CORES
+// ===============================
 
 function gerarCorAleatoria(){
     const r = Math.floor(Math.random() * 256)
     const g = Math.floor(Math.random() * 256)
     const b = Math.floor(Math.random() * 256)
-
+    
     return `rgb(${r},${g},${b})`
 }
-
+    
 cor_fundo.addEventListener('click', () => {
     bodyHTML.style.backgroundColor = gerarCorAleatoria()
     console.log("Cor alterada para: " + gerarCorAleatoria());
@@ -29,4 +37,23 @@ cor_titulo.addEventListener('click', () => {
 cor_paragrafo.addEventListener('click', () => {
     paragrafo.style.color = gerarCorAleatoria()
     console.log("Cor alterada para: " + gerarCorAleatoria());
+})
+
+// ===============================
+// FUNÇÕES PARA ALTERAR FONTES
+// ===============================
+
+function gerarFonteAleatoria(){
+    const fonteIndice =  Math.floor(Math.random() * 15)
+    return fontes[fonteIndice]
+}
+
+fonte_titulo.addEventListener('click', () => {
+    titulo.style.fontFamily = gerarFonteAleatoria()
+    console.log("Fonte alterada para " + gerarFonteAleatoria());
+})
+
+fonte_paragrafo.addEventListener('click', () => {
+    paragrafo.style.fontFamily = gerarFonteAleatoria()
+    console.log("Fonte alterada para " + gerarFonteAleatoria());
 })
